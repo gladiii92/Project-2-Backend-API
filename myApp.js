564ffe099e08c0e7ba8553ae5229399af0d74f4d
gladiii92 <Dave921@gmx.de>
2025-09-18 14:05:32 +0200

    feat: updated json v2

--- myApp.js
+++ myApp.js
@@ -9,15 +9,12 @@ app.get('/', function(req, res) {
 });
 
 app.get('/json', function(req, res) {
-  let message = "Hello json";
-
-  
-  if (process.env.MESSAGE_STYLE === "uppercase") {
-    message = message.toUpperCase();
-  }
-
+  const message = process.env.MESSAGE_STYLE === "uppercase"
+    ? "HELLO JSON"
+    : "Hello json";
   res.send(`{"message": "${message}"}`);
 });
+
 
 app.use('/public', express.static(__dirname + '/public'))
 
