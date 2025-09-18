af78f86e9c96659ca3544cc1e1bd2d5ebe5c2dfd
gladiii92 <Dave921@gmx.de>
2025-09-18 13:28:26 +0200

    feat: added api

--- myApp.js
+++ myApp.js
@@ -6,6 +6,10 @@ app.get('/', function(req, res) {
     res.sendFile(absolutePath);    
 });
 
+app.get('/json', function(req, res) {
+  res.json({ message: "Hello json" });
+});
+
 app.use('/public', express.static(__dirname + '/public'))
 
 console.log("Hello World");
