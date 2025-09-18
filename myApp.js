e6c8001daec22e5682deb36076066303a42afd06
gladiii92 <Dave921@gmx.de>
2025-09-18 15:45:18 +0200

    feat: json v3

--- myApp.js
+++ myApp.js
@@ -2,19 +2,20 @@ require('dotenv').config()
 
 let express = require('express');
 let app = express();
+app.set('json spaces', ' ');
 
 app.get('/', function(req, res) {
     const absolutePath = __dirname + '/views/index.html';
     res.sendFile(absolutePath);    
 });
 
-app.get('/json', function(req, res) {
-  const message = process.env.MESSAGE_STYLE === "uppercase"
-    ? "HELLO JSON"
-    : "Hello json";
-  res.send(`{"message": "${message}"}`);
+app.get('/json', (req, res) => {
+  let message = 'Hello json';
+  if (process.env.MESSAGE_STYLE === 'uppercase') {
+    message = message.toUpperCase();
+  }
+  res.json({ message });
 });
-
 
 app.use('/public', express.static(__dirname + '/public'))
 
