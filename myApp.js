779c6d66ec69b723c10f1dd988adbc8221169756
gladiii92 <Dave921@gmx.de>
2025-09-18 13:39:44 +0200

    feat: added uppercase and .env

--- myApp.js
+++ myApp.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 let express = require('express');
 let app = express();
 
@@ -7,7 +9,14 @@ app.get('/', function(req, res) {
 });
 
 app.get('/json', function(req, res) {
-  res.json({ message: "Hello json" });
+  let message = "Hello json";
+
+  
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    message = message.toUpperCase();
+  }
+
+  res.json({ message: message });
 });
 
 app.use('/public', express.static(__dirname + '/public'))
