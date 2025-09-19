e678bb0f18b6d4c4c6de0ee17a8d2fbddaefe053
gladiii92 <Dave921@gmx.de>
2025-09-19 12:00:42 +0200

    feat: next

--- myApp.js
+++ myApp.js
@@ -1,58 +1,31 @@
-require('dotenv').config()
+require('dotenv').config();
 
 let express = require('express');
 let app = express();
 
+// ✅ Root-Level Middleware zuerst
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
+
+// ✅ static middleware als nächstes
+app.use('/public', express.static(__dirname + '/public'));
+
+// ✅ dann die Routen
 app.get('/', function(req, res) {
-    const absolutePath = __dirname + '/views/index.html';
-    res.sendFile(absolutePath);    
+  const absolutePath = __dirname + '/views/index.html';
+  res.sendFile(absolutePath);    
 });
 
-app.get("/json",(req,res) => {
- let response;
-  if(process.env['MESSAGE_STYLE'] === "uppercase"){
-   response = "Hello json".toUpperCase();
-  }else{
-    response = "Hello json";
+app.get("/json", (req, res) => {
+  let response = "Hello json";
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    response = response.toUpperCase();
   }
-  res.json({"message": response});
+  res.json({ "message": response });
 });
-
-app.use('/public', express.static(__dirname + '/public'))
 
 console.log("Hello World");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 module.exports = app;
