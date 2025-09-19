3e5df269610301362c7d530c329c037ca5718a29
gladiii92 <Dave921@gmx.de>
2025-09-19 12:40:13 +0200

    feat: updated /now time

--- myApp.js
+++ myApp.js
@@ -3,16 +3,16 @@ require('dotenv').config();
 let express = require('express');
 let app = express();
 
-// ✅ Root-Level Middleware zuerst
+
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.path} - ${req.ip}`);
   next();
 });
 
-// ✅ static middleware als nächstes
+
 app.use('/public', express.static(__dirname + '/public'));
 
-// ✅ dann die Routen
+
 app.get('/', function(req, res) {
   const absolutePath = __dirname + '/views/index.html';
   res.sendFile(absolutePath);    
@@ -28,6 +28,17 @@ app.get("/json", (req, res) => {
       message: "Hello json"
     });
 });
+
+app.get('/now', 
+  function(req, res, next){
+    req.time = new Date().toString();
+    next();
+  },
+  function(req,res){
+    res.json({time: req.time})
+  }
+);
+
 
 console.log("Hello World");
 
